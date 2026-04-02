3539c97218c8614b7bc90d9538722d1929b164e3
Yesmakhan Ibraim <esmahan2001@mail.ru>
2026-04-02 16:01:36 +0300

    Add GET /notes/{note_id} endpoint

--- app/main.py
+++ app/main.py
@@ -18,3 +18,10 @@ def health():
 @app.get("/notes")
 def get_notes():
     return notes
+
+@app.get("/notes/{note_id}")
+def get_note(note_id: int):
+    for note in notes:
+        if note["id"] == note_id:
+            return note
+    return {"error" : "Note not found"}
