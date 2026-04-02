980e5488e811ae1fa5f84f24f5bf15f0a8e08bdc
Yesmakhan Ibraim <esmahan2001@mail.ru>
2026-04-02 17:19:33 +0300

    Add Basemodel schema

--- app/main.py
+++ app/main.py
@@ -1,46 +1,68 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class NoteCreate(BaseModel):
+    title: str
+    done: bool = False
+
+
 notes = [
-    {"id" : 1, "title" : "Learn FastAPI", "done" : False},
-    {"id" : 2, "title" : "Learn Git", "done" : False},
+    {"id": 1, "title": "Learn FastAPI", "done": False},
+    {"id": 2, "title": "Learn Git", "done": False},
 ]
+
 
 @app.get("/")
 def root():
-    return {"message": "My first backend app"}
+    return {"message": "Hello from FastAPI"}
+
 
 @app.get("/health")
 def health():
-    return {"status" : "Healthy"}
+    return {"status": "ok"}
+
 
 @app.get("/notes")
 def get_notes():
     return notes
+
 
 @app.get("/notes/{note_id}")
 def get_note(note_id: int):
     for note in notes:
         if note["id"] == note_id:
             return note
-    return {"error" : "Note not found"}
+    return {"error": "Note not found"}
+
 
 @app.post("/notes")
-def create_note(note: dict):
+def create_note(note: NoteCreate):
     new_note = {
-        "id" : len(notes) + 1,
-        "title" : note["title"],
-        "done" : note.get("done", False)
+        "id": len(notes) + 1,
+        "title": note.title,
+        "done": note.done
     }
     notes.append(new_note)
     return new_note
 
+
 @app.put("/notes/{note_id}")
-def update_note(note_id: int, updated_note: dict):
+def update_note(note_id: int, updated_note: NoteCreate):
     for note in notes:
         if note["id"] == note_id:
-            note["title"] = updated_note.get("title", note["title"])
-            note["done"] = updated_note.get("done", note["done"])
+            note["title"] = updated_note.title
+            note["done"] = updated_note.done
             return note
-    return {"error" : "Note not found"}
+    return {"error": "Note not found"}
+
+
+@app.delete("/notes/{note_id}")
+def delete_note(note_id: int):
+    for index, note in enumerate(notes):
+        if note["id"] == note_id:
+            deleted_note = notes.pop(index)
+            return deleted_note
+    return {"error": "Note not found"}
