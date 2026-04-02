3ea7dc0c285e1e6d72cb3e9dbad411dd85502075
Yesmakhan Ibraim <esmahan2001@mail.ru>
2026-04-02 15:56:14 +0300

    Add GET /notes endpoint

--- app/main.py
+++ app/main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+notes = [
+    {"id" : 1, "title" : "Learn FastAPI", "done" : False},
+    {"id" : 2, "title" : "Learn Git", "done" : False},
+]
+
 @app.get("/")
 def root():
-    return {"message": "Hello from FastAPI"}
+    return {"message": "My first backend app"}
 
 @app.get("/health")
 def health():
-    return {"status" : "ok"}
+    return {"status" : "Healthy"}
+
+@app.get("/notes")
+def get_notes():
+    return notes
